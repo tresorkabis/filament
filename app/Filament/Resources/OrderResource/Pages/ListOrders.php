0712bd51cd1ad51e16d80eb560f8d07a2953f6d6
Tresor <tresorkabis@gmail.com>
2025-03-16 22:22:19 +0100

    maj

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource;
 use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -22,6 +23,18 @@ class ListOrders extends ListRecords
     {
         return [
             OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn($query) => $query->where('status', 'new')),
+            'processing' => Tab::make()->query(fn($query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make()->query(fn($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make()->query(fn($query) => $query->where('status', 'delivered')),
+            'cancelled' => Tab::make()->query(fn($query) => $query->where('status', 'cancelled')),
         ];
     }
 }
