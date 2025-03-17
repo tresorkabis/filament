d6b7214582b03879709956ef1cf605482fe2e42c
Tresor <tresorkabis@gmail.com>
2025-03-17 16:03:18 +0100

    init livewire

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'DCodeMania' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+
+<body class="bg-slate-200 dark:bg-slate-700">
+    <main>
         {{ $slot }}
-    </body>
+    </main>
+    @livewireScripts
+</body>
+
 </html>
