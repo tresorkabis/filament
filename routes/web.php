9600eb3bb6da3255b69778bba540bfe0c0d48205
Tresor <tresorkabis@gmail.com>
2025-03-17 18:41:17 +0100

    cart static

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
