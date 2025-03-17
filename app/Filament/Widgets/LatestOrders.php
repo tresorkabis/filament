ae384da600c2d9d4077424021a07ead696586af5
Tresor <tresorkabis@gmail.com>
2025-03-17 15:27:36 +0100

    Panel stat

--- app/Filament/Widgets/LatestOrders.php
+++ app/Filament/Widgets/LatestOrders.php
@@ -14,6 +14,8 @@ class LatestOrders extends BaseWidget
 
     protected int | string | array $columnSpan = 'full';
 
+    protected static ?int $sort = 2;
+
     public function table(Table $table): Table
     {
         return $table
