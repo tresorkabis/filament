faff91f8a4304d53c1a70297926d4f4e259d2510
Tresor <tresorkabis@gmail.com>
2025-03-17 19:07:53 +0100

    ecommerce static page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Livewire\Auth\LoginPage;
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +16,9 @@ Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/cart', CartPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+
+Route::get('/login', LoginPage::class);
