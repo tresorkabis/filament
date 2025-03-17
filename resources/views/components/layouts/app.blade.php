e302e5545a515bd56ef109a5271c0f94408d55b8
Tresor <tresorkabis@gmail.com>
2025-03-17 18:21:40 +0100

    init homepage

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,9 +11,11 @@
 </head>
 
 <body class="bg-slate-200 dark:bg-slate-700">
+    @livewire('partials.navbar')
     <main>
         {{ $slot }}
     </main>
+    @livewire('partials.footer')
     @livewireScripts
 </body>
 
