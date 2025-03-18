5d6514a196b4f9fa7fc5a6d02ffecf998b0a6c6b
Tresor <tresorkabis@gmail.com>
2025-03-18 10:16:04 +0100

    categories page dynamique

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use App\Models\Category;
 
+#[Title('Categories - DCodeMania')]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::where('is_active', 1)->get();
+
+        return view('livewire.categories-page', [
+            'categories' => $categories,
+        ]);
     }
 }
